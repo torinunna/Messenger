d58fe29fa5fc7db374a8949d97c62949834e0e7d
torinunna <torinunna9588@gmail.com>
2024-04-15 16:44:01 +0900

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by YUJIN KWON on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
